3fb4e1b9cdb836d4df014203392699768659677b
coder <anthony.john.fawcett@gmail.com>
2020-05-16 11:37:36 +1000

    Adding some styles.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import './card.styles.css';
 
-export const Card = props => <h1>{props.cardData.title}</h1>;
+export const Card = props => <div className="card-container"><h1>{props.cardData.title}</h1></div>;
