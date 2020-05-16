51e87335e6273700453a619212451756b6a7f09b
Anthony Fawcett <anthonyjohnfawcett@gmail.com>
2020-05-16 15:44:08 +1000

    Updating github pages

---
+++ precache-manifest.6b936b073d66799cc2ee482c36153f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98db3998d7563e79caa9c190700d2a5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "aa705c24c58172b5c473",
+    "url": "/monster-rolodex/static/css/main.1673ad77.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa705c24c58172b5c473",
+    "url": "/monster-rolodex/static/js/main.f8c54c90.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  }
+]);
