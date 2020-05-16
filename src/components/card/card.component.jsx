587c1c0ac5bc42fd76a9ff9ad56ae6c1e2a15490
coder <anthony.john.fawcett@gmail.com>
2020-05-16 13:29:09 +1000

    Missed a spot

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import './card.styles.css';
 export const Card = ({cardData}) => 
     <div className="card-container">
         <h1 className="card-title">{cardData.title}</h1>
-        <img className="card-image" src={`http://robohash.org/${props.cardData.id}?set=set4&size=180x180`} alt={props.cardData.title} />
+        <img className="card-image" src={`http://robohash.org/${cardData.id}?set=set4&size=180x180`} 
+            alt={cardData.title} />
         <p className="card-motto"><strong>Motto: </strong>{cardData.motto}</p>
     </div>;
